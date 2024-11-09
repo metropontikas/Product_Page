8e7a84fb9a71b8db5ace453d64cb185e6f0d9ec2
metropontikas <grafomaipantou@tutanota.com>
2024-11-10 00:31:48 +0200

    Remove swipe feature

--- generate-related-products.js
+++ generate-related-products.js
@@ -13,46 +13,7 @@ const relatedProductsList = [
         image: "https://www.ilgusto.gr/wp-content/uploads/2024/11/e23-20238-34-dsc_0665_1_0-510x510.jpeg",
         name: "Σχετικό Προϊόν",
         price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/05/0t3a9646-1-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/02/0t3a8489-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/04/0t3a9251-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/06/0t3a9337-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-        {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/04/0t3a9251-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/06/0t3a9337-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/04/0t3a9251-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
-    {
-        image: "https://www.ilgusto.gr/wp-content/uploads/2024/06/0t3a9337-510x510.jpg",
-        name: "Σχετικό Προϊόν",
-        price: "€49.99",
-    },
+    }
 ];
 
 const relatedProducts = document.getElementById("relatedProducts");
@@ -60,7 +21,7 @@ const relatedProducts = document.getElementById("relatedProducts");
 for (let i = 0; i < relatedProductsList.length; i++) {
     const product = relatedProductsList[i];
     const productElement = document.createElement("div");
-    productElement.classList.add(...["col-md-3","gallery-item"]);
+    productElement.classList.add(...["col-md-3","product"]);
     productElement.innerHTML = `
         <div class="card">
             <img src="${product.image}" class="card-img-top" alt="${product.name}">
